595640fc45a9fd9b3097a692b6a4a0e01f837657
licky <895260021@qq.com>
2020-12-04 23:01:50 +0800

    外观数列

--- src/main/java/com/licky/leetcode/Solution.java
+++ src/main/java/com/licky/leetcode/Solution.java
@@ -1,7 +1,8 @@
 package com.licky.leetcode;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Solution {
@@ -90,10 +91,6 @@ public class Solution {
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-     *
-     * @param nums1
-     * @param nums2
-     * @return
      */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         var length = nums1.length + nums2.length;
@@ -154,36 +151,62 @@ public class Solution {
 
     }
 
+    /**
+     * 给定一个正整数 n ，输出外观数列的第 n 项。
+     * <p>
+     * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+     * <p>
+     * 你可以将其视作是由递归公式定义的数字字符串序列：
+     * <p>
+     * countAndSay(1) = "1"
+     * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+     * 前五项如下：
+     * <p>
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     * 第一项是数字 1
+     * 描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+     * 描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+     * 描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+     * 描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+     * 要 描述 一个数字字符串，首先要将字符串分割为 最小 数量的组，每个组都由连续的最多 相同字符 组成。然后对于每个组，先描述字符的数量，然后描述字符，形成一个描述组。要将描述转换为数字字符串，先将每组中的字符数量用数字替换，再将所有描述组连接起来。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/count-and-say
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public static String countAndSay(int n) {
         if (n == 1) {
             return "1";
         }
-
         var string = countAndSay(n - 1);
 
-        Queue<Character> chars = string.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toCollection(LinkedList::new));
-
-        Character nowChar = Character.MIN_VALUE;
+        var chars = string.toCharArray();
+        var start = 0;
+        var end = 0;
         var count = 0;
-        StringBuilder result = new StringBuilder();
-        do {
-            var poll = chars.poll();
-            if (nowChar == poll) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (; end < chars.length; end++) {
+            if (chars[start] == chars[end]) {
                 count++;
             } else {
-                result.append(count).append(nowChar);
-                count = 0;
-                nowChar = poll;
+                builder.append(count).append(chars[start]);
+                start = end;
+                count = 1;
             }
-        } while (nowChar != null);
-        return result.toString();
+            if (end == chars.length - 1) {
+                builder.append(count).append(chars[start]);
+            }
+        }
 
+        return builder.toString();
     }
 
     public static void main(String[] args) {
-        var re = countAndSay(2);
-        System.out.println(re);
     }
 }
