a64f33d1133a820c98bc072a2b7c910c52949ebf
licky <895260021@qq.com>
2020-12-08 21:26:08 +0800

    7

--- src/main/java/com/licky/aoc/Day7.java
+++ src/main/java/com/licky/aoc/Day7.java
@@ -13,22 +13,9 @@ import java.util.stream.Collectors;
 public class Day7 implements AOCBase {
 
     public void partOne(String input) {
-        var bagLines = input.split("\n");
-        Set<Bag> all = new HashSet<>(bagLines.length);
-        for (String bagLine : bagLines) {
+        var all = getBags(input);
 
-            var bags = bagLine.split("bags contain");
-            var bag = new Bag(bags[0].trim());
-            if (!bags[1].contains("no other bags")) {
-                var subBags = bags[1].replaceAll("[,.0-9]", "").split("bags|bag");
-                for (String subBag : subBags) {
-                    bag.getSubBags().add(new Bag(subBag.trim()));
-                }
-            }
-            all.add(bag);
-        }
-
-        var subBags = Set.of(new Bag("shiny gold"));
+        var subBags = Set.of(new Bag("shiny gold", 1));
         var result = new HashSet<Bag>();
         do {
             Set<Bag> finalSubBags = subBags;
@@ -42,7 +29,7 @@ public class Day7 implements AOCBase {
     }
 
 
-    private static final Bag gold = new Bag("shiny gold");
+    private static final Bag gold = new Bag("shiny gold", 1);
 
     private boolean containsGold(Bag bag, Set<Bag> all) {
         if (bag.getSubBags() == null || bag.getSubBags().size() == 0) {
@@ -54,18 +41,82 @@ public class Day7 implements AOCBase {
                         containsGold(
                                 all.stream().filter(a -> a.equals(sub))
                                         .findAny()
-                                        .orElseGet(() -> new Bag("")),
+                                        .orElseGet(() -> new Bag("", 1)),
                                 all
                         )
                 );
 
     }
 
+    private int getSubBagsCount(Bag bag, Set<Bag> all, boolean root) {
+        if (root && bag.getSubBags().size() == 0) {
+            return 0;
+        } else if (bag.getSubBags().size() == 0) {
+            return bag.getCount();
+        }
+
+        var count = bag.getSubBags().stream().map(sub ->
+                sub.getCount() * getSubBagsCount(
+                        all.stream().filter(a -> a.equals(sub))
+                                .findAny().get(),
+                        all,
+                        false
+                )
+        ).reduce(0, Integer::sum) + bag.getCount();
+
+        return count;
+
+    }
+
+    private int some(Bag bag, Set<Bag> all) {
+        if (bag.getSubBags() == null || bag.getSubBags().size() == 0) {
+            return bag.getCount();
+        }
+
+        var count = bag.getSubBags().stream()
+                .map(sub ->
+                        sub.getCount() * some(
+                                all.stream().filter(a -> a.equals(sub))
+                                        .findAny()
+                                        .orElseGet(() -> new Bag("", 1)),
+                                all
+                        )
+                ).reduce(0, Integer::sum) + bag.getCount();
+        return count;
+    }
+
+    public Set<Bag> getBags(String input) {
+        var bagLines = input.split("\n");
+        Set<Bag> all = new HashSet<>(bagLines.length);
+        for (String bagLine : bagLines) {
+
+            var bags = bagLine.split("bags contain");
+            var bag = new Bag(bags[0].trim(), 1);
+            if (!bags[1].contains("no other bags")) {
+                var subBags = bags[1].replaceAll("[,.]", "").split("bags|bag");
+                for (String subBag : subBags) {
+                    var b = subBag.trim().split(" ");
+                    bag.getSubBags().add(new Bag(b[1] + " " + b[2], Integer.parseInt(b[0])));
+                }
+            }
+            all.add(bag);
+        }
+        return all;
+    }
+
     public void partTwo(String input) {
+        var all = getBags(input);
+        var count = all.stream().filter(bag -> bag.equals(gold))
+                .findAny()
+                .map(bag ->
+                        getSubBagsCount(bag, all, true)
+                )
+                .get()-1;
+        System.out.println(count);
     }
 
     public static void main(String[] args) {
-        AOCBase.runPartOne(Day7::new, 7);
+        AOCBase.runPartTwo(Day7::new, 7);
     }
 
 
@@ -73,9 +124,14 @@ public class Day7 implements AOCBase {
     @FieldDefaults(level = AccessLevel.PRIVATE)
     public static class Bag {
 
-        public Bag(String color) {
+        public Bag(String color, int count) {
             this.color = color;
+            this.count = count;
         }
+
+        @ToString.Exclude
+        @EqualsAndHashCode.Exclude
+        int count;
 
         String color;
 
